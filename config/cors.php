0d13da7866c5b994c64f3eb349ce9f47fb1d0c8a
Zenythic <gestionservicios@zenythic.com>
2026-03-15 15:19:34 -0500

    final update cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'], // importante por tus rutas sin /api
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://smarthouse-sve.com', 'https://www.smarthouse-ve.com'],
+    'allowed_origins' => ['https://smarthouse-ve.com', 'https://www.smarthouse-ve.com'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
